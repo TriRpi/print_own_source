7466d020a967ee1a41ee5a56a3d3429325761a39
Tristan Trouwen <tristan.trouwen@gmail.com>
2016-07-30 13:42:36 +0200

    last r prints now

--- print_source.py
+++ print_source.py
@@ -1,3 +1,3 @@
 
-a = r"print 'a = \"{}\"'.format(a) + '\n' +  a"
-print 'a = r\"{}\"'.format(a) + '\n' +  a
+a = r"print 'a = r\"{}\"'.format(a) + '\n' +  a"
+print "a = r\"{}\"'.format(a) + '\n' +  a
